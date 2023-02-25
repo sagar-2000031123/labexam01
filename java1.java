bee9928d0612dc8150c8d3b54e291e0f89e5a438
2000031123 <2000031123@kluniversity.in>
2023-02-25 11:55:16 +0530

    iam sagar

---
+++ java1.java
@@ -0,0 +1,10 @@
+class add
+{
+public static void main(String []args)
+{
+int a = 1;
+int b = 2;
+int sum = a+b;
+system.out.println("a + "+" + b + "=" +sum")
+}
+} 
---
+++ python.py
@@ -0,0 +1,4 @@
+num1 = 8
+num2 = 5
+sum = (num1 + num2)
+print('The sum{0} and {1} or {2},format(num1,num2,sum'))
